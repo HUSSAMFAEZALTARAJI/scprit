2aaf5d8c9e2f9e267ebe7795315d984b8f241130
HUSSAMFAEZALTARAJI <60089237+HUSSAMFAEZALTARAJI@users.noreply.github.com>
2020-04-11 14:18:41 +0300

    Add files via upload

---
+++ 2-2.py
@@ -0,0 +1,34 @@
+# print('Hello python')
+# print('Wlecom from python')
+
+
+# x = 3.5
+# print(type(x))
+
+# y = '20'
+# print(type(y))
+
+
+# Name = 'My name : Hussam  '
+
+# Age = 'My age : 21'
+
+# print(Name,Age)
+
+
+
+
+# H = '18'
+# T = 'book'
+# print(H+T)
+
+
+
+# X = 15
+# Y = 30
+# print(X/Y)
+
+
+
+
+
